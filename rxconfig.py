7303b1fe6141103579f7ab777314290b5b01d38d
NexoCodAI <wikas123987@gmail.com>
2025-12-19 12:42:46 +0100

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="rx_pau_tracker.py", # Reemplaza con el nombre exacto de tu carpeta de código
+    app_name="pau_elite", # Reemplaza con el nombre exacto de tu carpeta de código
 )
