3fc9ad054f03f341a636178d36092bb2be81bd8f
NexoCodAI <wikas123987@gmail.com>
2025-12-19 13:38:26 +0100

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,5 +3,5 @@ import reflex as rx
 config = rx.Config(
     app_name="pau_elite",
     # REEMPLAZA ESTO CON TU URL REAL DE RAILWAY
-    api_url="https://paupublico-production.up.railway.app/",
+    #api_url="https://paupublico-production.up.railway.app/",
 )
